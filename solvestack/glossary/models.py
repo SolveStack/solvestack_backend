d7a338587a0c3dc5df4b754c5e9b52e384589ea9
Tom Boolean <tomboolean@gmail.com>
2020-11-21 10:56:10 -0700

    remove technology node for lesson

--- solvestack/glossary/models.py
+++ solvestack/glossary/models.py
@@ -7,9 +7,3 @@ class Term(models.Model):
     id = ShortUUIDField(primary_key=True, editable=False)
     name = models.CharField(max_length=80)
     definition = models.TextField()
-
-class TechnologyNode(models.Model):
-    id = ShortUUIDField(primary_key=True, editable=False)
-    name = models.CharField(max_length=80)
-    definition = models.CharField(max_length=255)
-    similar_tech = models.TextField()
--- solvestack/glossary/serializers.py
+++ solvestack/glossary/serializers.py
@@ -1,16 +1,10 @@
 from rest_framework import serializers
 
-from solvestack.glossary.models import Term, TechnologyNode
+from solvestack.glossary.models import Term
 
 
 class TermSerializer(serializers.ModelSerializer):
     class Meta:
         model = Term
         fields = ["id", "name", "definition"]
-        read_only_fields = ["id"]
-
-class TechnologyNodeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = TechnologyNode
-        fields = ["id", "name", "definition", "similar_tech"]
         read_only_fields = ["id"]
